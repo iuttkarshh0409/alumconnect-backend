852d60330e71bb7b99c31bb0137fdbbad35f31b2
iuttkarshh0409 <dubeutkarsh7@gmail.com>
2026-03-04 23:51:30 +0530

    Clean repo: ignore venv and pycache

---
+++ test.py
@@ -0,0 +1,4 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+client = AsyncIOMotorClient("mongodb+srv://dubeyutkarsh101:REALPASSWORD@alumconnect.p6ntqnf.mongodb.net/alumconnect?retryWrites=true&w=majority")
+print("Connected")
